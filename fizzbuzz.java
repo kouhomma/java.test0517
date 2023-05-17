55fe3f7d0abb55c9d029f46e3aa83c59e56243b5
kouhomma <k-homma@virtualex.co.jp>
2023-05-17 10:01:21 +0900

    First Commit

---
+++ fizzbuzz.java
@@ -0,0 +1,22 @@
+package FizzBuzz;
+
+import java.util.Scanner;
+
+public class fizzbuzz {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int x = s.nextInt();
+		if (x % 15 == 0) {
+			System.out.println("FizzBuzz");
+		} else if (x % 3 == 0) {
+			System.out.println("Fizz");
+		} else if (x % 5 == 0) {
+			System.out.println("Buzz");
+		} else {
+			System.out.println(x);
+		}
+
+	}
+
+}
